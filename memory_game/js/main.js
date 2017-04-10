b334910590a821b1620edb94b80cce46b9412999
Simon Amor <simonamor@SIMON-AMOR.local>
2017-04-10 20:18:58 +0100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("user flipped " + cardTwo);
+console.log("user flipped " + cardThree);
