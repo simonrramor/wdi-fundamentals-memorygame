d6cd59d85b2e0dc59b9aaf67f97f9d7f57b6412d
Simon Amor <simonamor@SIMON-AMOR.local>
2017-04-12 18:39:51 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,54 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	
+	{
+       rank: 'queen',
+       suit: 'hearts',
+       cardImage: "images/queen-of-hearts.png"
+  	},
+
+  	{
+       rank: 'queen',
+       suit: 'diamonds',
+       cardImage: "images/queen-of-diamonds.png"
+  	},
+
+  	{
+       rank: 'king',
+       suit: 'hearts',
+       cardImage: "images/king-of-hearts.png"
+  	},
+
+  	{
+       rank: 'king',
+       suit: 'diamonds',
+       cardImage: "images/king-of-diamonds.png"
+  	}
+ ];
 
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log ("user flipped " + cardOne);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log ("user flipped " + cardTwo);
-
- if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
+var checkForMatch = function (){
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
   } else {
       alert("Sorry, try again.");
-  }
+	}
+}
+
+
+var flipCard = function (cardID) {
+	console.log("user flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	checkForMatch()
+
+	console.log(cards[cardID].cardImage)
+	console.log(cards[cardID].suit)
+}
+
+flipCard(0)
+flipCard(2)
+
+
+
+
