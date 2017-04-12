2de3ed30412e530e0c18a98209c3a95910965d9c
Simon Amor <simonamor@simon-amor.home>
2017-04-12 11:08:22 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,17 @@
-console.log("up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("user flipped " + cardTwo);
-console.log("user flipped " + cardThree);
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log ("user flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log ("user flipped " + cardTwo);
+
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+  } else {
+      alert("Sorry, try again.");
+  }
