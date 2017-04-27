55a74eeaec2e76e09130b2c753141809a8bf3380
Simon Amor <simonamor@SIMON-AMOR.local>
2017-04-27 12:17:36 +0100

    final upload for GA

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-// Create an array to store all card objects
+
 var cards = [
   {
     rank: 'queen',
@@ -19,15 +19,38 @@ var cards = [
     rank: 'king',
     suit: 'diamonds',
     cardImage: 'images/king-of-diamonds.png'
+  },
+
+  {
+    rank: 'jack',
+    suit: 'diamonds',
+    cardImage: 'images/jack-of-spades.png'
+  },
+
+  {
+    rank: 'jack',
+    suit: 'diamonds',
+    cardImage: 'images/jack-of-clubs.png'
+  },
+  {
+    rank: 'jack',
+    suit: 'diamonds',
+    cardImage: 'images/ace-of-dimonds.png'
+  },
+
+  {
+    rank: 'jack',
+    suit: 'diamonds',
+    cardImage: 'images/ace-of-hearts.png'
   }
 ];
 
-// Create an array to store the cards in play
+
+
 var cardsInPlay = [];
 
-// Create a function to check for a match
 var checkForMatch = function () {
-  // Check to see if two cards match and provide feedback to user
+  
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
@@ -35,53 +58,42 @@ var checkForMatch = function () {
   }
 };
 
-// Create a function to represent a user flipping a card
 var flipCard = function () {
-  // Get the data-id of the card that was just flipped and cache it
+
   cardId = this.getAttribute('data-id');
   console.log(cardId);
 
-  // Add rank to array of cards that are in play
   cardsInPlay.push(cards[cardId].rank);
-
-  // Display the card's image
   this.setAttribute('src', cards[cardId].cardImage);
-
-  // Check to see if two cards have been played
   if (cardsInPlay.length === 2) {
-    // If so, call the checkForMatch function
     checkForMatch();
-    // Empty cards in play array for next try
     cardsInPlay = [];
   }
-};
 
-// Function that will create your board
-var createBoard = function () {
-  // Loop through your cards array to create card elements for your board
-  for (var i = 0; i < cards.length; i++) {
-    // Create an img element which will be used as a card
+};
+    var createBoard = function () {
+      
+    cards = cards.sort(function(a, b){return 0.5 - Math.random()});
+
+    for (var i = 0; i < cards.length; i++) {
+
     var cardElement = document.createElement('img');
 
-    // Set the src attribute to display the back of card image
+
     cardElement.setAttribute('src', 'images/back.png');
 
-    // Set the card's 'data-id' attribute to be the index of the current element
-    // data- attributes are meant to store data about an element that is not tied to a style.
+    
     cardElement.setAttribute('data-id', i);
 
-    // Add an event listener so that when a card is clicked, 
-    // the function flipCard will be executed
+  
     cardElement.addEventListener('click', flipCard);
 
-    // Append the card to the board
     document.getElementById('game-board').appendChild(cardElement);
-
   }
 
-}
 
-// Call the createBoard function to create the board.
+
+}
 createBoard();
 
 
